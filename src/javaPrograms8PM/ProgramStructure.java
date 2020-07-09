287b2062191535f8c05082b3c2d1e587133d1da6
Sonal <mittal.sonal04@gmail.com>
2020-07-08 20:59:06 -0400

    Signed-off-by: Sonal <mittal.sonal04@gmail.com>

---
+++ src/javaPrograms8PM/ProgramStructure.java
@@ -0,0 +1,25 @@
+package javaPrograms8PM;
+
+public class ProgramStructure {
+
+	// Any code written outside main method will not be executed
+	
+	// My first program -- comments
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// it is a method available in java 
+// main method is used in java to execute our program
+		// Write the code inside the main() method
+		
+		//ANy java program
+		// 1. write the program
+		//2. compiling a program   
+		// 3. executing the program
+		
+	// Every statement in Java program will end with ;
+		
+		System.out.println("Welcome to class");
+		
+	}
+
+}
---
+++ src/javaPrograms8PM/VariableJava.java
@@ -0,0 +1,86 @@
+package javaPrograms8PM;
+
+public class VariableJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      
+		// Variables and Datatypes in Java
+		
+		//Whenever we are working with data on webPages which is
+		// fetching data from a webpage
+		// Send some data
+		// store data and print the data to verification of test cases
+		
+		// Variables : they are temporary locations to store data
+		
+		//Varible syntax : data types uniquevariablename = data;
+		// 2 types of datatypes -- primitive and non primitive
+		
+		// primitive data types include: int, float , boolean and double , char, byte , short
+		
+		// Non primitive data types: String , Arrays
+		// data types associated with  the variables
+		// we also need to provide the type os data the variable is storing
+		// Data types: int : this data type is used when a variable is storing integer values
+		//
+		
+		//variable declaration
+		int a1;
+		// assigning value to variables
+		a1=200;
+		
+		String p1="200";
+		
+		int a_=10;
+		
+		int b$=2345;
+		
+		char c3='%';
+		String c4="How are you?%67&";
+		
+		System.out.println(b$);  // this statement will print the value of b in console
+		
+		int s4=1245677;
+		// String = if a variable is storing a series of character than we will use data types as String
+		// String always represnted in double quotes
+		String s1="Selenium trainign at 8 PM";
+		
+		String s2="1234567";
+		
+		
+		
+		//if a variable is storing decimal values than data type will be float or double
+		float dec=2.344546f;
+		double d1= 3.45534565465756765678;
+		
+		
+	boolean bl= true;
+		
+	char ch= 'A';
+	char ch1= '1';	
+		
+		int b1=100;
+	
+	// changing the value of a variable			
+		
+
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+	}
+
+}
+
+
+
+
+
+
